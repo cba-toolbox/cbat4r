9defd0fe6474b8caf85c90c531d028a5bf08bc76
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2026-07-08 10:37:27 +0900

    完了コードの記載を修正

--- inst/templates/tasks/completion_code.js
+++ inst/templates/tasks/completion_code.js
@@ -3,10 +3,10 @@ const id_no = jsPsych.randomization.randomID(10);
 
 const finish = {
   type: jsPsychHtmlButtonResponse,
-  stimulus: '<p style="font-size:30px;">研究に参加いただき，誠にありがとうございました。</p>' +
-    '<p style="font-size:30px;">これですべての課題とアンケートが終了しました。</p>' +
+  stimulus: '<p style="font-size:30px;">これで研究参加は終了になります。</p>' +
+  　'<p style="font-size:30px;">研究にご参加いただき，誠にありがとうございました。</p>' +
     '<p style="font-size:30px;">あなたの参加IDは，<span style="color:#6495ED;"> ' + id_no + ' </span>です</p>' +
-    '<p style="font-size:20px;">紙にメモし，参加IDをコピーしてから，以下の「終了」をクリックして終了ください</p>',
+    '<p style="font-size:20px;">同意撤回時に必要になりますので、紙にメモし，参加IDをコピーしてから，以下の「終了」をクリックして終了ください</p>',
   choices: ['終了'],
   data: {id: id_no}
 };
