744c122283c85ff88ec75261f1750a1f488fa4af
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2026-07-09 13:46:11 +0900

    update

--- R/phaser.R
+++ R/phaser.R
@@ -5,6 +5,8 @@ phaser_html <- function(phaser_version) {
     "<!DOCTYPE html>",
     "<html>",
     " <head>",
+    '  <meta charset="UTF-8"/>',
+    '  <meta name="viewport" content="width=device-width,initial-scale=1.0"/>',
     paste0('  <script src="//cdn.jsdelivr.net/npm/phaser@', phaser_version,
            '/dist/phaser.js"></script>'),
     " </head>",
